9e6dd92c2d8c665cb8a95444e14fef2e6177679f
Jose Gimeno Niza <jgn_2001@hotmail.com>
2021-11-18 14:24:31 +0100

    a

--- Fighter2D_Grupo_3_Unity/Assets/Scripts/SoundScripts/AudioManager.cs
+++ Fighter2D_Grupo_3_Unity/Assets/Scripts/SoundScripts/AudioManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour{
 
     public Sound[] sounds;
+    public static AudioManager instance;
     // Start is called before the first frame update
     void Awake()
     {
